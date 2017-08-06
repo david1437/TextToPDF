a443a935890900e4e220981820afe3953c6c0b1f
david1437 <davidsherrier1997@gmail.com>
2017-08-05 22:02:49 -0400

    Updated README.md added file reading

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TextToPDF
+doc folder contains PDF ISO standard that will be implemented
+compile by typing make in the main directory
+bin contains the executable
+currently outputs data from file test/text.txt
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int main() {
-	cout << "TextToPDF version 0.1" << endl;
+	string line;
+	ifstream rf("data/test.txt");
+	if(rf.is_open()) {
+		while(getline(rf, line))
+		{
+			cout << line << '\n';
+		}
+		rf.close();
+	}
+	else {
+		cout << "Error accured!" << '\n';
+	}
 }
 

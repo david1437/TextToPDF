2d0245f7128a8a9bd3b9a960197a4ce45405247c
david1437 <davidsherrier1997@gmail.com>
2017-08-05 20:42:27 -0400

    Added cmake support

---
+++ CMakeFiles/TextToPDF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextToPDF.dir/src/main.cpp.o"
+  "TextToPDF.pdb"
+  "TextToPDF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextToPDF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/bin/TextToPDF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin/TextToPDF.dir/src/main.cpp.o"
+  "bin/TextToPDF.pdb"
+  "bin/TextToPDF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin/TextToPDF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
